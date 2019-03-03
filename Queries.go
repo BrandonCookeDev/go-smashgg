362fb19e3e682d944ba3c423c632192cd72da6d7
unknown <BrandonCookeDev@gmail.com>
2019-03-02 21:42:54 -0500

    Renamed

---
+++ Queries.go
@@ -0,0 +1,65 @@
+package main
+
+var tournamentQuery = `query TournamentQuery($slug: String){
+	tournament(slug: $slug){
+		id
+		name
+		slug
+		city
+		postalCode
+		addrState
+		countryCode
+		region
+		venueAddress
+		venueName
+		gettingThere
+		lat
+		lng
+		timezone
+		startAt
+		endAt
+		contactInfo
+		contactEmail
+		contactTwitter
+		contactPhone
+		ownerId
+	}	
+}`
+
+var eventQuery = `query EventQuery($slug: String){
+	event(slug: $slug){
+		id
+		name
+		slug
+		state
+		startAt
+		numEntrants
+		checkInBuffer
+		checkInDuration
+		checkInEnabled
+		isOnline
+		teamNameAllowed
+		teamManagementDeadline
+	}	
+}`
+
+var phaseQuery = `query PhaseQuery($id: Int){
+	phase(id: $id){
+		id
+		name
+		numSeeds
+		groupCount
+	}	
+}`
+
+var phaseGroupQuery = `query PhaseGroupQuery($id: Int){
+	phaseGroup(id: $id){
+		id
+		displayIdentifier
+		firstRoundTime
+		state
+		phaseId
+		waveId
+		tiebreakOrder
+	}	
+}`
